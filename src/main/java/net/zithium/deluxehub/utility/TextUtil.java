044d7132ad28d03880d906e89f48300be296765d
RootBeer <rootb33rman@gmail.com>
2025-11-23 05:20:07 -0500

    Refactor branding

---
+++ src/main/java/net/zithium/deluxehub/utility/TextUtil.java
@@ -0,0 +1,56 @@
+package net.zithium.deluxehub.utility;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+
+import java.util.List;
+
+public class TextUtil {
+
+    public static String fromList(List<?> list) {
+        if (list == null || list.isEmpty()) return null;
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (ChatColor.stripColor(list.get(i).toString()).isEmpty()) {
+                builder.append("\n&r");
+            } else {
+                builder.append(list.get(i).toString()).append(i + 1 != list.size() ? "\n" : "");
+            }
+        }
+
+        return builder.toString();
+    }
+
+    public static String joinString(int index, String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = index; i < args.length; i++) {
+            builder.append(args[i]).append(" ");
+        }
+
+        return builder.toString();
+    }
+
+    public static Color getColor(String s) {
+        return switch (s.toUpperCase()) {
+            case "AQUA" -> Color.AQUA;
+            case "BLACK" -> Color.BLACK;
+            case "BLUE" -> Color.BLUE;
+            case "FUCHSIA" -> Color.FUCHSIA;
+            case "GRAY" -> Color.GRAY;
+            case "GREEN" -> Color.GREEN;
+            case "LIME" -> Color.LIME;
+            case "MAROON" -> Color.MAROON;
+            case "NAVY" -> Color.NAVY;
+            case "OLIVE" -> Color.OLIVE;
+            case "ORANGE" -> Color.ORANGE;
+            case "PURPLE" -> Color.PURPLE;
+            case "RED" -> Color.RED;
+            case "SILVER" -> Color.SILVER;
+            case "TEAL" -> Color.TEAL;
+            case "WHITE" -> Color.WHITE;
+            case "YELLOW" -> Color.YELLOW;
+            default -> null;
+        };
+    }
+}
